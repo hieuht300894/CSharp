18980bf1746303967e106530cb0d7feacc9e5a5a
HIEU HOANG <HIEU_HOANG@jabil.com>
2018-01-31 14:41:01 +0700

    1

---
+++ GameBlackJack/GameBlackJack/GUI/frmBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameBlackJack.GUI
+{
+    public partial class frmBase : Form
+    {
+        public frmBase()
+        {
+            InitializeComponent();
+
+            Load -= FrmBase_Load;
+            Load += FrmBase_Load;
+        }
+
+        protected virtual void FrmBase_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ GameBlackJack/GameBlackJack/GUI/frmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameBlackJack.GUI
+{
+    public partial class frmMain : frmBase
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void FrmBase_Load(object sender, EventArgs e)
+        {
+            base.FrmBase_Load(sender, e);
+
+
+        }
+    }
+}
